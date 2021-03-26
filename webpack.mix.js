70ef2e7df9440d099d8b0e6790d30893dddc6c1f
sqlazo <emequispe@gmail.com>
2021-03-26 10:49:09 -0500

    Me salvare??

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
-const mix = require("laravel-mix");
-const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
+const mix = require('laravel-mix');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 
-const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 var webpackConfig = {
-  plugins: [new VuetifyLoaderPlugin(), new CaseSensitivePathsPlugin()]
+  plugins: [new VuetifyLoaderPlugin(), new CaseSensitivePathsPlugin()],
 };
 
 mix.webpackConfig(webpackConfig);
@@ -20,6 +20,7 @@ mix.webpackConfig(webpackConfig);
  */
 
 mix
-  .js("resources/js/app.js", "public/js")
+  .js('resources/js/app.js', 'public/js')
   .vue()
-  .sass("resources/sass/app.scss", "public/css");
+  .sass('resources/sass/app.scss', 'public/css')
+  .version();
