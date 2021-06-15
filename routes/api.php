049242dd1a9abca32fab9b73fb011872d6336d18
Mathi03 <mathi031@hotmail.com>
2021-06-15 00:30:35 -0500

    Feat: Route to products availables

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ use App\Http\Controllers\Api\v1\RoleController;
 use App\Http\Controllers\Api\v1\OrderShippingStatusController;
 use App\Http\Controllers\Api\v1\SalePaymentController;
 use App\Http\Controllers\Api\v1\PermissionController;
+use App\Http\Controllers\Api\v1\SaleProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,15 +41,16 @@ use App\Http\Controllers\Api\v1\PermissionController;
 //     return $request->user();
 // });
 
-Route::group(['prefix' => 'v1'], function () {
+Route::group(['middleware' => ['cors', 'json.response'], 'prefix' => 'v1'], function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('signup', [AuthController::class, 'signup']);
     Route::get('catalogues-availables', [CatalogueController::class, 'availables']);
+    Route::get('products-availables', [SaleProductController::class, 'availables']);
     Route::get('banks', [BankEntityController::class, 'index']);
     Route::get('advisers', [AdviserController::class, 'index']);
     
     // Las siguientes rutas además del prefijo requieren que el usuario tenga un token válido
-    Route::group(['middleware' => ['cors', 'auth:api', 'json.response']], function() {
+    Route::group(['middleware' => ['auth:api']], function() {
         Route::get('logout', [AuthController::class, 'logout']);
         Route::get('user', [AuthController::class, 'user']);
         Route::put('user-update', [AuthController::class, "update"]);
